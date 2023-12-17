ee5e09f2ce0c2b38bc1d1e7f72722e3a51688400
赵鑫 <zhaoxin@hualala.com>
2023-12-17 23:19:22 +0800

    feat: user and blog

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -31,7 +31,30 @@ const getDetail = (id) => {
   ]
 }
 
+const newBlog = (blogData = {}) => {
+  // blogData 是一个博客对象，包含 title, content 属性
+  // console.log('newBlog Data', blogData)
+  return {
+    id: 3
+  }
+}
+
+const updateBlog = (id, blogData = {}) => {
+  console.log(id, blogData, 'update ===')
+  // id 就是要更新博客的 id
+  // blogData 是一个博客对象，包含 title, content 属性
+  return true
+}
+
+const delBlog = (id) => {
+  // id 就是要删除博客的 id
+  return true
+}
+
 module.exports = {
   getList,
-  getDetail
+  getDetail,
+  newBlog,
+  updateBlog,
+  delBlog
 }
--- src/router/blog.js
+++ src/router/blog.js
@@ -1,8 +1,10 @@
-const { getList, getDetail } = require('../controller/blog')
+const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 const handleBlogRouter = (req, res) => {
+  console.log(req.query, 'req info')
   const method = req.method
+  const id = req.query.id
 
   // 获取博客列表
   if (method === 'GET' && req.path === '/api/blog/list') {
@@ -14,28 +16,32 @@ const handleBlogRouter = (req, res) => {
 
   // 获取博客详情
   if (method === 'GET' && req.path === '/api/blog/detail') {
-    const id = req.query.id
     const data = getDetail(id)
     return new SuccessModel(data)
   }
 
   // 新建博客
   if (method === 'POST' && req.path === '/api/blog/new') {
-    return {
-      msg: '这是新建博客的接口'
-    }
+    const data = newBlog(req.body)
+    return new SuccessModel(data)
   }
 
   // 更新博客
   if (method === 'POST' && req.path === '/api/blog/update') {
-    return {
-      msg: '这是更新博客的接口'
+    const result = updateBlog(id, req.body)
+    if (result) {
+      return new SuccessModel()
+    } else {
+      return new ErrorModel("更新失败")
     }
   }
 
   if (method === 'POST' && req.path === '/api/blog/del') {
-    return {
-      msg: '这是删除博客的接口'
+    const result = delBlog(id)
+    if (result) {
+      return new SuccessModel()
+    } else {
+      return new ErrorModel("删除博客失败")
     }
   }
 }
